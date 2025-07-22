7e80c4f66fa812689353fe946b2a1f66b615d409
Gustavo Campo de Souza <160344662+Gustavoo-Campos@users.noreply.github.com>
2025-07-22 10:24:45 -0300

    HTML & CSS 100%

--- src/index.js
+++ src/index.js
@@ -2,20 +2,6 @@
 
 /* - - - - - - - - - THEME - - - - - - - -  */
 
-const toggleTheme = document.getElementById('theme')
-const rootHtml = document.documentElement
-
-function changeTheme() {
-    const currentTheme = rootHtml.getAttribute("data-theme");
-
-   currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
-}
-toggleTheme.addEventListener("click", changeTheme);
-
-
-
-
-
 
 
 /* - - - - - - - - - NAV - - - - - - - -  */
